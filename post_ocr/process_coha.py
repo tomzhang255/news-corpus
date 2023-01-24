ae6b91afa1cf62a313f95fa4257e76ae9c2c2951
tomzhang255 <tomzhang255@gmail.com>
2023-01-24 01:13:36 -0500

    method 1 implemented

--- post_ocr/process_coha.py
+++ post_ocr/process_coha.py
@@ -27,4 +27,4 @@ dic = dic.loc[dic['nchar'] >= 3]
 dic.drop('nchar', axis=1, inplace=True)
 
 # save
-dic.to_csv('../data/coha_dic.csv', index=False)
+dic.to_csv('../data/coha_dict.csv', index=False)
